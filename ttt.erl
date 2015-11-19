a30dec05120a718fe2529a1f721bcee71412a97f
Charles Daniel <charles.sam.daniel@gmail.com>
2015-11-18 22:10:09 -0500

    Breaking up complex functions, making things more Erlangy. Relates to issue #1

--- ttt.erl
+++ ttt.erl
@@ -1,13 +1,18 @@
 -module ttt.
 
 -author "Charles Daniel <charles.sam.daniel@gmail.com>".
--vsn "0.1".
+-vsn "0.2".
 
 -export([start_game/0]).
 
-%% Print the board, the board looks like [{1,2,3},{4,5,6},{7,8,9}] (any of those can also be the atom x or o)
+-define(USER, "X").
+-define(COMPUTER, "O").
+
+% Print the board, the board looks like [{1,2,3},{4,5,6},{7,8,9}] (any of those can also be the values of ?USER or ?COMPUTER)
 print_board({A,B,C}) ->
-    io:format("~p | ~p | ~p ~n", [A, B, C]);
+    % Print a single row
+    % Note if it's an integer we'll need to convert it to the equivalent string before printing
+    io:format("~s | ~s | ~s ~n", [ case is_integer(X) of true -> integer_to_list(X); _->X end || X <- [A, B, C] ]);
 
 print_board([]) ->
     ok;
@@ -23,7 +28,7 @@ print_board([H|T]) ->
     end.
 
 
-%% Return a new Board with the piece in the Move position if valid otherwise return an invalid_move
+% Return a new Board with the piece in the Move position if valid otherwise return an invalid_move
 place_piece(Board, Piece, Move) ->
     case lists:any(fun(X) -> case X of {Move, _, _} -> true; {_, Move, _}->true; {_, _, Move}-> true; _->false end end, Board) of
         true ->
@@ -33,32 +38,31 @@ place_piece(Board, Piece, Move) ->
     end.
 
 
-%% Prompt the user for a move after printing the board out
-get_user_move(Board, Piece) ->
+% Prompt the user for a move after printing the board out
+get_move(Board, ?USER) ->
     print_board(Board),
     {ok, [Move]} = io:fread("Your Move -> ", "~d"),
+    Move;
+
+% Get a move for the computer (We just choose a random location for now)
+% TODO: Make this smarter
+get_move(_Board, ?COMPUTER) ->
+    rand:uniform(9).
+
+
+% Call a get_move for the player until they give us a valid empty spot
+process_move(Board, Piece) ->
+    Move = get_move(Board, Piece),
     case place_piece(Board, Piece, Move) of
         {ok, NewBoard} ->
             NewBoard;
         {invalid_move, _} ->
-            io:format("Illegal Move\n"),
-            get_user_move(Board, Piece)
+            io:format("Illegal Move ~n"),
+            process_move(Board, Piece)
     end.
 
 
-%% Get a move from the computer (it's just random for now)
-%% TODO: Make computer smarter
-get_computer_move(Board, Piece) ->
-    Move = rand:uniform(9),
-    case place_piece(Board, Piece, Move) of
-        {ok, NewBoard} ->
-            NewBoard;
-        {invalid_move, _} ->
-            get_computer_move(Board, Piece)
-    end.
-
-
-%% Figure out if there was a winner and return who it was
+% Figure out if there was a winner and return who it was
 calculate_winner(Board) ->
     case Board of
         [{A, A, A}, {_, _, _}, {_, _, _}] -> A;
@@ -73,34 +77,30 @@ calculate_winner(Board) ->
     end.
 
 
-%% Recursively run the game (take a turn each time)
-%% If we're at more than 9 moves it's a tie
+% Quick function to flip the turn to the other player
+whos_next(?USER) -> ?COMPUTER;
+whos_next(?COMPUTER) -> ?USER.
+
+
+% Main driver, runs the game and checks for maximum number of moves (to call it a Tie)
+run_game(Board, _, Moves) when Moves > 9 ->
+    print_board(Board),
+    io:format("It's a Tie! ~n");
+
 run_game(Board, WhosTurn, Moves) ->
-    if
-        Moves > 9 ->
-            print_board(Board),
-            io:format("It's a Tie\n");
-        true ->
-            {NewBoard, WhosTurnNext} = case WhosTurn of
-                user1 -> {get_user_move(Board, x), computer};
-                computer -> {get_computer_move(Board, o), user1};
-                _ -> {invalid_move}
-            end,
-            case calculate_winner(NewBoard) of
-                x ->
-                    print_board(NewBoard),
-                    io:format("Winner is X (you)\n");
-                o ->
-                    print_board(NewBoard),
-                    io:format("Winner is O (computer)\n");
-                _ -> run_game(NewBoard, WhosTurnNext, Moves + 1)
-            end
+    NewBoard = process_move(Board, WhosTurn),
+    case calculate_winner(NewBoard) of
+        not_done ->
+            run_game(NewBoard, whos_next(WhosTurn), Moves + 1);
+        A ->
+            print_board(NewBoard),
+            io:format("Winner is ~s! ~n", [A])
     end.
 
 run_game(Board, WhosTurn) ->
     run_game(Board, WhosTurn, 1).
 
 
-%% Entry point into the game
+% Entry point into the game
 start_game() ->
-    run_game([{1, 2, 3}, {4, 5, 6}, {7, 8, 9}], user1).
+    run_game([{1, 2, 3}, {4, 5, 6}, {7, 8, 9}], ?USER).
